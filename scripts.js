178196b29378efacd2ca8949b18d51b6a16671fa
Monika N <monia.niemczyk1@gmail.com>
2018-04-11 15:26:31 +0200

    Add 'return'

--- scripts.js
+++ scripts.js
@@ -1,12 +1,9 @@
 function getTriangleArea(a, h) {
 	if ( (a > 0) && (h > 0) ) {
-		
+		return a*h/2;
 	} else {
-		console.log("Nieprawidłowe dane")
-
+		return "Nieprawidłowe dane";
 	};
-
-	return a*h/2
 };
 
 console.log(getTriangleArea(10, 6));
